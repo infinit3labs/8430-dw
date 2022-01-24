835c1ba42e76374ad410b9c9fa3cfcfe5ac39443
X13\u4587 <u4587426@gmail.com>
2022-01-24 15:08:50 +1100

    clean medical

--- medical/postcode.py
+++ medical/postcode.py
@@ -18,10 +18,37 @@ df = pd.read_csv('medical_clean.csv', dtype=str)
 
 pc = pd.read_csv('australian_postcodes.csv', dtype=str)
 
-pc = pc[['postcode', 'locality']]
+pc = pc[['postcode', 'locality', 'state']]
 
 pc1 = pc.groupby('postcode')['locality'].apply(list)
 
 df = df.replace(np.nan, '', regex=True)
 
 df['pcode_check'] = df.apply(lambda row: pcode_valid(row), axis=1)
+
+def suburb_valid(row):
+    if row['postcode'] != '' and row['suburb'] != '' and row['state'] != '':
+        pcode_src = row['postcode']
+        suburb_src = row['suburb']
+        state_src = row['state']
+        src_key = suburb_src + state_src
+        try:
+            postcode_lookup = pc2.loc[pc2['key'] == src_key]['postcode'].values[0]
+            if pcode_src == postcode_lookup:
+                return 1
+            else:
+                return 0
+        except IndexError:
+            return 'invalid locality'
+
+pc2 = pc
+pc2['key'] = pc.locality + pc.state
+pc2['key'] = pc2.key.str.lower()
+
+
+df['suburb_check'] = df.apply(lambda row: suburb_valid(row), axis=1)
+
+t = df.loc[df['rec_id'] == 'rec-49632'].copy()
+t['suburb_check'] = t.apply(lambda row: suburb_valid(row), axis=1)
+
+print('x')
