38f9d7e9aa7efa9b05f4acc17ac0dfe50c558e7f
X13\u4587 <u4587426@gmail.com>
2022-01-24 23:08:26 +1100

    more cleaning - names

---
+++ medical/names.py
@@ -0,0 +1,52 @@
+#%%
+import os
+# Source data from: https://www.ssa.gov/oact/babynames/limits.html :: National data
+t = os.listdir('names')
+print(t)
+
+#%%
+import pandas as pd
+li = []
+
+for filename in t:
+    df = pd.read_csv('names/'+filename, index_col=None, header=None)
+    li.append(df)
+
+frame = pd.concat(li, axis=0, ignore_index=True)
+
+#%%
+frame.columns = ['name', 'gender', 'count']
+#%%
+names = frame.values.tolist()
+#%%
+n_dict = {}
+
+for i in names:
+    if f'{i[0]}_{i[1]}' in n_dict:
+        n_dict[f'{i[0]}_{i[1]}'] += i[2]
+    else:
+        n_dict[f'{i[0]}_{i[1]}'] = i[2]
+#%%
+n_dict_a = sorted(n_dict)
+#%%
+import operator
+n_dict_a = sorted(n_dict.items(), key=operator.itemgetter(1))
+#%%
+n_dict_2 = {}
+for a, b in n_dict_a:
+    name = a.split('_')[0].lower()
+    gender = a.split('_')[1].lower()
+    n_dict_2[name] = gender
+#%%
+l = []
+for i in n_dict_2.items():
+    m = list(i)
+    l.append(m)
+
+
+#%%
+import csv
+with open('names.csv', 'w', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerows(l)
+#%%
